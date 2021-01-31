4b26f8bd047b03dabc3d39c01c722844a0a40a19
ktonyi <antonigorevich.k@gmail.com>
2021-02-01 01:16:52 +0300

    add new code

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
 public class Main {
     public  static  void  main ( String [ ]  args ) {
         int currentBalance = 100;
-        int deposit = 1001;
+        int deposit = 2000;
         int totalbonus = deposit/100;
         int newBalance = currentBalance + deposit + totalbonus;
-        if (deposit<1001)
+        if (deposit < 1001)
         {
-            System.out.println("Итоговый Бонус");
-            System.out.println("0");
-            System.out.println("Текущий Баланс");
-            System.out.println(deposit+currentBalance);
-
-        } else {System.out.println("Итоговый бонус");
-            System.out.println(totalbonus);
-            System.out.println("Текущий Баланс");
-            System.out.println(newBalance);
-    }
+            totalbonus = 0;
+        }
+        System.out.println("Ваш бонус составил:"+totalbonus+"р");
+        System.out.println("Текущий баланс:"+newBalance+"р");
 }
+
 }
