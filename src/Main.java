e0a020112fab254af444951d97b623d549fe581c
ktonyi <antonigorevich.k@gmail.com>
2021-02-04 12:49:45 +0300

    remake

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public  static  void  main ( String [ ]  args ) {
         int currentBalance = 100;
         int deposit = 2000;
-        int totalbonus = deposit/100;
-        int newBalance = currentBalance + deposit + totalbonus;
-        if (deposit < 1001)
+        int totalbonus = 0;
+
+                if (deposit > 1000)
         {
-            totalbonus = 0;
+            totalbonus = deposit/100;
         }
         System.out.println("Ваш бонус составил:"+totalbonus+"р");
-        System.out.println("Текущий баланс:"+newBalance+"р");
+        System.out.println("Текущий баланс:"+(currentBalance + deposit + totalbonus)+"р");
 }
 
 }
